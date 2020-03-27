89e80c222327f046c5154bf426f5a8e8c5848c31
Max Beddies <ubyjy@student.kit.edu>
2020-03-27 23:26:37 +0100

    Added auto test cases for constructors

--- test/FibonacciHeap/FibonacciHeapTest.cpp
+++ test/FibonacciHeap/FibonacciHeapTest.cpp
@@ -1,2 +1,9 @@
+#include <FibonacciHeap/FibonacciHeap.h>
+
 #define BOOST_TEST_MODULE fibonacci_heap test
 #include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(constructorTest)
+{
+    BOOST_CHECK_EQUAL(true, true);
+}
--- test/PairingHeap/PairingHeapTest.cpp
+++ test/PairingHeap/PairingHeapTest.cpp
@@ -1,3 +1,9 @@
+#include <PairingHeap/PairingHeap.h>
+
 #define BOOST_TEST_MODULE pairing_heap test
 #include <boost/test/unit_test.hpp>
 
+BOOST_AUTO_TEST_CASE(constructorTest)
+{
+    BOOST_CHECK_EQUAL(true, true);
+}
